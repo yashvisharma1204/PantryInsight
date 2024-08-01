a9e0fadbe248d16cc042dda261eb53c6ca5201c6
Yashvi Sharma <137611141+yashvisharma1204@users.noreply.github.com>
2024-08-02 02:41:21 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB5qOb_VGwhagMOvCU8U5wJB-gEXweLp6Y",
@@ -18,5 +18,7 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db = getFirestore(app);
+  const storage = getStorage(app);
 
-export { auth, db };
+
+export { auth, db, storage };
